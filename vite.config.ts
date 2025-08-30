a5b258fb4165d796aea8427f8722411486b7f520
zechmeister <flo@mzechmeister.de>
2025-08-30 18:30:07 +0200

    Set root to "/"

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/148.studio",
+  plugins: [react(), tailwindcss()],
 });
